4747c2264be10539d84b38830f8f8e5c4f045ca0
Ultraviolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-07-09 16:25:20 +0200

    add docs folder

---
+++ docs/source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Flask-CRUDGenerator'
+copyright = '2024, Ultraviolet33'
+author = 'Ultraviolet33'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "pallets_sphinx_themes",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "flask"
+html_static_path = ['_static']
