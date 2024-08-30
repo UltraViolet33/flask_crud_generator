724895b30b0bc4b68deba4468e47ab16b01d38f2
Ultraviolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-08-30 11:34:14 +0200

    edit conf.py readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Flask-CRUDGenerator'
 copyright = '2024, Ultraviolet33'
 author = 'Ultraviolet33'
-release = '0.1.0'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
