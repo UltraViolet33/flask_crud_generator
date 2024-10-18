163ae57038c790c837618526e87775becd7ae0dc
Ultraviolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-10-18 09:56:06 +0200

    create and edit route with wtf

--- flask_crud_generator/generator.py
+++ flask_crud_generator/generator.py
@@ -18,7 +18,7 @@ class CRUDGenerator:
             app.extensions = {}
         app.extensions["crud_generator"] = self
 
-    def generate_web_routes(self, model, blueprint=None, blueprint_name=None):
+    def generate_web_routes(self, model, blueprint=None, blueprint_name=None, form_class=None, populate_choices_func=None):
         self.copy_templates_to_app()
         model_name = model.__name__.lower()
 
@@ -42,35 +42,65 @@ class CRUDGenerator:
         
         @blueprint.route("/create/", methods=["GET", "POST"])
         def create_item_web():
-            if request.method == 'POST':
-                form_data = request.form.to_dict()
-                model_columns = {column.name for column in model.__table__.columns}
-                filtered_data = {key: form_data[key] for key in model_columns if key in form_data}
-                try:
-                    item = model(**filtered_data)                    
+            if form_class is not None:
+                form = form_class()
+                if populate_choices_func:
+                    populate_choices_func(form)
+                if form.validate_on_submit():
+                    data = form.data
+                    del data['csrf_token']
+                    item = model(**data)
                     self.db.session.add(item)
                     self.db.session.commit()
-                    return redirect(url_for(f"{blueprint_name}.list_items_web"))
-                except Exception as e:
-                    self.db.session.rollback()
-                    return render_template('create.html',  columns=model.__table__.columns)
-            return render_template('create.html',  columns=model.__table__.columns)
+                    # flash(f'{model_name.capitalize()} created successfully!', 'success')
+                    return redirect(url_for(f'{blueprint_name}.list_items_web'))
+                return render_template('wtf_create.html', form=form, model_name=model_name.capitalize(), action="Create")
+
+            else:
+                if request.method == 'POST':
+                    form_data = request.form.to_dict()
+                    model_columns = {column.name for column in model.__table__.columns}
+                    filtered_data = {key: form_data[key] for key in model_columns if key in form_data}
+                    try:
+                        item = model(**filtered_data)                    
+                        self.db.session.add(item)
+                        self.db.session.commit()
+                        return redirect(url_for(f"{blueprint_name}.list_items_web"))
+                    except Exception as e:
+                        self.db.session.rollback()
+                        return render_template('create.html',  columns=model.__table__.columns)
+                return render_template('create.html',  columns=model.__table__.columns)
 
         @blueprint.route("/edit/<int:item_id>", methods=["GET", "POST"])
         def edit_item_web(item_id):
             item = model.query.get_or_404(item_id)
-            if request.method == 'POST':
-                form_data = request.form.to_dict()
-                model_columns = {column.name for column in model.__table__.columns}
-                try:
-                    for key, value in form_data.items():
-                        if key in model_columns:
-                            setattr(item, key, value)
+            if form_class is not None:
+                form = form_class(obj=item)
+                if populate_choices_func:
+                    populate_choices_func(form)
+                if form.validate_on_submit():
+                    data = form.data
+                    del data['csrf_token']
+                    for key, value in data.items():
+                        setattr(item, key, value)
                     self.db.session.commit()
-                    return redirect(url_for(f"{blueprint_name}.list_items_web"))
-                except Exception as e:
-                    self.db.session.rollback()
-            return render_template('edit.html', columns=model.__table__.columns, item=item)
+                    # flash(f'{model_name.capitalize()} updated successfully!', 'success')
+                    return redirect(url_for(f'{model_name}.list_items_web'))
+                return render_template(f'wtf_create.html', form=form, model_name=model_name.capitalize(), action='Edit')
+            else:
+
+                if request.method == 'POST':
+                    form_data = request.form.to_dict()
+                    model_columns = {column.name for column in model.__table__.columns}
+                    try:
+                        for key, value in form_data.items():
+                            if key in model_columns:
+                                setattr(item, key, value)
+                        self.db.session.commit()
+                        return redirect(url_for(f"{blueprint_name}.list_items_web"))
+                    except Exception as e:
+                        self.db.session.rollback()
+                return render_template('edit.html', columns=model.__table__.columns, item=item)
             
         self.app.register_blueprint(blueprint, url_prefix=f"/{blueprint_name}")
 
@@ -127,6 +157,7 @@ class CRUDGenerator:
             os.makedirs(app_templates_dir)
 
         for filename in os.listdir(package_templates_dir):
+            # copy the files and directories 
             source_file = os.path.join(package_templates_dir, filename)
             destination_file = os.path.join(app_templates_dir, filename)
             
